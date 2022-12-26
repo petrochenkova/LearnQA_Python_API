a946f20f54193993325294b51c34fabed6707f59
Anastasiia Petrochenkova <a.petrochenkova@ibolit.pro>
2022-12-26 12:18:24 +0300

    Обратный слэш

--- lib/logger.py
+++ lib/logger.py
@@ -5,7 +5,7 @@ from requests import Response
 
 
 class Logger:
-    file_name = f"logs/log_" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + ".log"
+    file_name = f"logs\log_" + str(datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S")) + ".log"
 
     # Классметод позволяет обращаться к переменным класса через "cls"
     @classmethod
